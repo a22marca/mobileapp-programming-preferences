7f52b7ac78c771f630081a7acb72690b9e5f522b
a22marca <128985931+a22marca@users.noreply.github.com>
2023-05-02 23:06:15 +0200

    Implementerat SharedPreferences i MainActivity och SecondActivity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.project;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -11,12 +13,20 @@ import androidx.appcompat.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
 
     private Intent intent;
+
+    private TextView textView;
+    private SharedPreferences sharedPreferenceRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        sharedPreferenceRef = getPreferences(MODE_PRIVATE);
+        sharedPreferenceRef  = getSharedPreferences("SharedPreferenceName", MODE_PRIVATE);
+        textView = findViewById(R.id.textView);
+
 
         Button buttonSecondActivity = findViewById(R.id.buttonSecondActivity);
         intent = new Intent(MainActivity.this, SecondActivity.class);
@@ -26,6 +36,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+    }
+
+    public void onResume() {
+        super.onResume();
+        textView.setText(sharedPreferenceRef.getString("SharedPreferenceString","Empty"));
     }
 
 }
--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -2,13 +2,32 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private SharedPreferences sharedPreferenceRef;
+    private SharedPreferences.Editor sharedPreferenceEditor;
+
+    private  EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        sharedPreferenceRef = getPreferences(MODE_PRIVATE);
+        sharedPreferenceRef = getSharedPreferences("SharedPreferenceName",MODE_PRIVATE);
+        sharedPreferenceEditor = sharedPreferenceRef.edit();
+        editText = findViewById(R.id.editText);
+
+    }
+
+    public void onResume() {
+        super.onResume();
+        editText.setText(sharedPreferenceRef.getString("SharedPreferenceString","Empty"));
     }
 }
